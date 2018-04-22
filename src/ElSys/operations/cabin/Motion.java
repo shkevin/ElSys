853db1bc05c376f35612271b9e42dd009f029250
shkevin <shk3vin@yahoo.com>
2018-04-22 14:28:37 -0600

    Added door threads, need to be able to draw and decrement/increment the door values now.

--- src/ElSys/operations/cabin/Motion.java
+++ src/ElSys/operations/cabin/Motion.java
@@ -3,6 +3,10 @@ package ElSys.operations.cabin;
 import ElSys.interfaces.Motor;
 import ElSys.interfaces.FloorAlignment;
 import ElSys.operations.building.BuildSpecs;
+
+import java.util.ArrayList;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 /*
  * Each Cabin will start a Motion thread when it gets a command to move that will track where in the elevator
  * the cabin is and communicate with the MotorControl while running
@@ -13,18 +17,23 @@ public class Motion implements Runnable {
     private double currentFloor;
     private int targetFloor;
     private double speed;
-
     private String cabin;
-    public Thread t;
+    private Thread t;
     private MotionTypes motionType = MotionTypes.NOTMOVING;
     private boolean hasRequest = false;
     private Cabin cab;
     private Motor motionMotor = new Motor();
     private FloorAlignment floorAlignment = new FloorAlignment(this);
+    private CyclicBarrier barrier;
+    private ArrayList<DoorThread> threads;
+    double outerDoorVal = 0;
 
     public Motion(double startingFloor, int cabNum, Cabin cab) {
         this.currentFloor = startingFloor;
         this.cab = cab;
+        this.threads = new ArrayList<>(BuildSpecs.NUM_DOOR_THREADS);
+        initializeDoorThreads();
+        this.barrier = new CyclicBarrier(BuildSpecs.NUM_DOOR_THREADS, this::onBarrierBroken);
         switch (cabNum) {
             case 0:
                 this.cabin = "One";
@@ -42,6 +51,16 @@ public class Motion implements Runnable {
         System.out.println("(Motion) Creating thread for elevator: " + this.cabin);
         t = new Thread(this, ("motion " + cabin));
         t.start();
+    }
+
+    private void initializeDoorThreads() {
+        String name;
+        for (int i = 0; i < BuildSpecs.NUM_DOOR_THREADS; i++) {
+            if (i % 2 == 0) name = "InnerDoor";
+            else name = "OuterDoor";
+            DoorThread thread = new DoorThread(this.barrier, motionType, name);
+            threads.add(thread);
+        }
     }
 
     //must be called before move or starting the thread so the elevator knows where to go
@@ -149,28 +168,105 @@ public class Motion implements Runnable {
     }
 
     public void openDoors(){
-        try {
-            this.motionType = MotionTypes.DOORS;
-            this.speed = 0; //might eventually be doorsopening
-            Thread.sleep(BuildSpecs.doorSpeed);
-        } catch (InterruptedException e) {
-            System.out.println("Thread interrupted while opening doors");
-
-        }
+//        try {
+//            this.motionType = MotionTypes.DOORSOPENING;
+//            this.speed = 0;
+//            Thread.sleep(BuildSpecs.DOOR_SPEED);
+//        } catch (InterruptedException e) {
+//            System.out.println("Thread interrupted while opening doors");
+//
+//        }
+        this.motionType = MotionTypes.DOORSOPENING;
+        iterateDoorThreads(this.motionType);
     }
 
     private void closeDoors(){
-        try {
-            this.motionType = MotionTypes.DOORS; //might eventually be doorsclosing
-            Thread.sleep(BuildSpecs.doorSpeed);
-            this.motionType = MotionTypes.NOTMOVING;
-        } catch (InterruptedException e) {
-            System.out.println("Thread interrupted while closing doors");
+//        try {
+//            this.motionType = MotionTypes.DOORSCLOSING;
+//            Thread.sleep(BuildSpecs.DOOR_SPEED);
+//            this.motionType = MotionTypes.NOTMOVING;
+//        } catch (InterruptedException e) {
+//            System.out.println("Thread interrupted while closing doors");
+//        }
+        this.motionType = MotionTypes.DOORSCLOSING;
+        iterateDoorThreads(this.motionType);
+    }
+
+    private void iterateDoorThreads(MotionTypes motion) {
+        for (DoorThread thread : threads) {
+            if (thread != null) {
+                thread = new DoorThread(this.barrier, motion, thread.name);
+                thread.start();
+            }
         }
     }
 
+    private void onBarrierBroken() {
+        //Once animation is done, unlock/lock the doors
+        if (this.motionType == MotionTypes.DOORSOPENING) {
+            try {
+                this.speed = 0;
+//                Thread.sleep(BuildSpecs.DOOR_SPEED);
+                Thread.sleep(BuildSpecs.DOOR_SPEED);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted while opening doors");
+            }
+        }
+        else if (this.motionType == MotionTypes.DOORSCLOSING) {
+            try {
+                Thread.sleep(BuildSpecs.DOOR_SPEED);
+                this.motionType = MotionTypes.NOTMOVING;
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted while closing doors");
+            }
+        }
+        System.out.println("Done with doors");
+    }
 
     public FloorAlignment getFloorAlignment() {
         return floorAlignment;
+    }
+
+
+    /**
+     * Door Thread class.
+     */
+    private class DoorThread extends Thread {
+        private CyclicBarrier barrier;
+        private MotionTypes motion;
+        private String name;
+
+        DoorThread(CyclicBarrier barrier, MotionTypes motion, String name) {
+            this.barrier = barrier;
+            this.motion = motion;
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            try {
+
+                if (this.name.equalsIgnoreCase("InnerDoor")) {
+                    if (this.motion == MotionTypes.DOORSOPENING) {
+                        System.out.println("Opening inner");
+                    }
+                    else if (this.motion == MotionTypes.DOORSCLOSING) {
+                        System.out.println("Closing inner");
+                    }
+                }
+                else if (this.name.equalsIgnoreCase("OuterDoor")) {
+                    if (this.motion == MotionTypes.DOORSOPENING) {
+                        System.out.println("Opening outer");
+                    }
+                    else if (this.motion == MotionTypes.DOORSCLOSING) {
+                        System.out.println("Closing outer");
+                    }
+                }
+
+                barrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                System.out.println("Error occurred");
+            }
+        }
     }
 }
